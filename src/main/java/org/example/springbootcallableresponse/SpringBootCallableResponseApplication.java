191cc340fe575cafc36ead1128e11b1777645dc1
이수민 <smlee@twolinecode.com>
2024-06-20 12:13:59 +0900

    first commit

---
+++ src/main/java/org/example/springbootcallableresponse/SpringBootCallableResponseApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootcallableresponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCallableResponseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCallableResponseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbootcallableresponse/SpringBootCallableResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootcallableresponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCallableResponseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
